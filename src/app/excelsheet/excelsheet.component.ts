723fe88389e6d20f65ca64f8970b336d8f361f58
Bryantonelly <bryan.antonelly.villegas.cuba@gmail.com>
2023-07-28 22:31:01 -0500

    cambio color celdas y contador

--- src/app/excelsheet/excelsheet.component.ts
+++ src/app/excelsheet/excelsheet.component.ts
@@ -15,11 +15,11 @@ export class ExcelsheetComponent implements OnInit {
   selectedOption: string;
 
   valoresJovenMe1er = [
-    { codigo: "Z000", labconf: "" },
-    { codigo: "C8002", labconf: "1" },
-    { codigo: "99403.01", labconf: "1" },
+    { codigo: "Z000", color:"#D2BF55",  labconf: "" },
+    { codigo: "C8002", color:"#FFEED6", labconf: "1" },
+    { codigo: "99403.01", color:"#4CE0B3", labconf: "1" },
     { codigo: "96150.01", labconf: "" },
-    { codigo: "99402.09", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" },
     { codigo: "Z019", labconf: "DNT" },
     { codigo: "E46X", labconf: "IMC" },
     { codigo: "Z006", labconf: "IMC" },
@@ -31,7 +31,7 @@ export class ExcelsheetComponent implements OnInit {
     { codigo: "99401.16", labconf: "" },
     { codigo: "Z010", labconf: "N" },
     { codigo: "Z011", labconf: "N" },
-    { codigo: "99401", labconf: "1" },
+    { codigo: "99401", color:"#85CB33", labconf: "1" },
     { codigo: "99173", labconf: "20" },
     { codigo: "99173", labconf: "20" },
     { codigo: "Z128", labconf: "N" },
@@ -49,40 +49,41 @@ export class ExcelsheetComponent implements OnInit {
     { codigo: "84478", labconf: "" }
   ];
   valoresJovenOtros1er = [
-    { codigo: "99402.08", labconf: "1" },
-    { codigo: "99208", labconf: "" },
-    { codigo: "99402.04", labconf: "1" },
-    { codigo: "99402.03", labconf: "1" },
-    { codigo: "88141", labconf: "" },
+    { codigo: "99402.08", color:"#E16036", labconf: "1" },
+    { codigo: "99208", color:"#8CA0D7", labconf: "" },
+    { codigo: "99402.04", color:"#9D79BC", labconf: "1" },
+    { codigo: "99402.03", color:"#91C4F2", labconf: "1" },
+    { codigo: "88141", color:"#D6A99A", labconf: "" },
     { codigo: "VACUNA", labconf: "" },
-    { codigo: "C0011", labconf: "1" },
-    { codigo: "99401", labconf: "" },
-    { codigo: "99402.09", labconf: "" }
+    { codigo: "C0011", color:"9D79BC", labconf: "1" },
+    { codigo: "99401", color:"#85CB33", labconf: "" },
+    { codigo: "96150.04", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" }
   ];
   valoresJovenDr2do = [
-    { codigo: "Z000", labconf: "" },
-    { codigo: "C8002", labconf: "TA" },
-    { codigo: "96150.02", labconf: "" },
-    { codigo: "99402.09", labconf: "" },
-    { codigo: "99403.01", labconf: "2" },
-    { codigo: "99401", labconf: "2" },
-    { codigo: "U292", labconf: "DNT" }
+    { codigo: "Z000", color:"#D2BF55",  labconf: "" },
+    { codigo: "C8002", color:"#FFEED6", labconf: "TA" },
+    { codigo: "96150.02", color:"#FBBFCA", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" },
+    { codigo: "99403.01", color:"#4CE0B3", labconf: "2" },
+    { codigo: "99401", color:"#85CB33", labconf: "2" },
+    { codigo: "U292", color:"#F7D002", labconf: "DNT" }
   ];
   valoresJovenOtros2do = [
-    { codigo: "99208", labconf: "" },
-    { codigo: "99402.04", labconf: "2" },
-    { codigo: "99402.03", labconf: "2" },
-    { codigo: "88141", labconf: "N" },
-    { codigo: "99402.08", labconf: "2" },
-    { codigo: "C0011", labconf: "2" },
-    { codigo: "99401", labconf: "" },
-    { codigo: "96150.03", labconf: "" },
-    { codigo: "99402.09", labconf: "" }
+    { codigo: "99208", color:"#8CA0D7", labconf: "" },
+    { codigo: "99402.04", color:"#9D79BC", labconf: "2" },
+    { codigo: "99402.03", color:"#91C4F2", labconf: "2" },
+    { codigo: "88141", color:"#D6A99A", labconf: "N" },
+    { codigo: "99402.08", color:"#E16036", labconf: "2" },
+    { codigo: "C0011", color:"#C8AB83", labconf: "2" },
+    { codigo: "99401", color:"#85CB33", labconf: "" },
+    { codigo: "96150.03", color:"#55868C", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" }
   ];
 
   valoresAdultoDr1er = [
-    { codigo: "Z000", labconf: "" },
-    { codigo: "C8002", labconf: "1" },
+    { codigo: "Z000", color:"#D2BF55",  labconf: "" },
+    { codigo: "C8002", color:"#FFEED6", labconf: "1" },
     { codigo: "Z019", labconf: "DNT" },
     { codigo: "E46X", labconf: "IMC" },
     { codigo: "Z006", labconf: "IMC" },
@@ -92,19 +93,19 @@ export class ExcelsheetComponent implements OnInit {
     { codigo: "U8170", labconf: "RSA" },
     { codigo: "U8170", labconf: "RMA" },
     { codigo: "99199.22", labconf: "N" },
-    { codigo: "99401.13", labconf: "" },
-    { codigo: "99401", labconf: "" },
+    { codigo: "99401.13", color:"#bb8588", labconf: "" },
+    { codigo: "99401", color:"#85CB33", labconf: "" },
     { codigo: "Z017", labconf: "" },
     { codigo: "82947", labconf: "" },
     { codigo: "84478", labconf: "" },
     { codigo: "82465", labconf: "" },
     { codigo: "85018", labconf: "1" },
     { codigo: "81003", labconf: "" },
-    { codigo: "99401", labconf: "1" },
-    { codigo: "99403.01", labconf: "1" },
+    { codigo: "99401", color:"#85CB33", labconf: "1" },
+    { codigo: "99403.01", color:"#4CE0B3", labconf: "1" },
     { codigo: "Z128", labconf: "N" },
     { codigo: "96150.01", labconf: "" },
-    { codigo: "99402.09", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" },
     { codigo: "99401.33", labconf: "" },
     { codigo: "86703.01", labconf: "RN" },
     { codigo: "99401.34", labconf: "" },
@@ -115,55 +116,55 @@ export class ExcelsheetComponent implements OnInit {
     { codigo: "99401.16", labconf: "" },
     { codigo: "Z010", labconf: "N" },
     { codigo: "Z011", labconf: "N" },
-    { codigo: "84152", labconf: "" },
+    { codigo: "84152", color:"#edc531", labconf: "" },
     { codigo: "99386.03", labconf: "N" },
-    { codigo: "82270", labconf: "" }
-  ];
-  valoresAdultoOtros1er = [
-    { codigo: "88141", labconf: "N" },
-    { codigo: "99402.08", labconf: "2" },
-    { codigo: "96150.03", labconf: "" },
-    { codigo: "99402.09", labconf: "2" },
-    { codigo: "99402.03", labconf: "2" },
-    { codigo: "99402.04", labconf: "2" },
-    { codigo: "C0011", labconf: "2" },
-    { codigo: "99401", labconf: "" }
-  ];
-  valoresAdultoDr2do = [
-    { codigo: "Z000", labconf: "" },
-    { codigo: "C8002", labconf: "TA" },
-    { codigo: "99403.01", labconf: "2" },
-    { codigo: "U262", labconf: "DNT" },
-    { codigo: "84152", labconf: "RN" },
-    { codigo: "99401.13", labconf: "" },
-    { codigo: "99401", labconf: "2" },
-    { codigo: "96150.02", labconf: "2" },
-    { codigo: "99402.09", labconf: "2" },
-    { codigo: "82270", labconf: "N" },
-    { codigo: "U0041", labconf: "" }
+    { codigo: "82270", color:"#a3a380", labconf: "" }
   ];
   valoresAdultoOtros2do = [
-    { codigo: "88141", labconf: "N" },
-    { codigo: "88141.01", labconf: "N" },
-    { codigo: "99402.08", labconf: "1" },
-    { codigo: "99402.04", labconf: "1" },
-    { codigo: "99402.06", labconf: "1" },
-    { codigo: "99402.03", labconf: "1" },
-    { codigo: "99208", labconf: "" },
-    { codigo: "90714", labconf: "1" },
-    { codigo: "90658", labconf: "" },
-    { codigo: "90749.02", labconf: "" },
-    { codigo: "C0011", labconf: "1" },
-    { codigo: "99401", labconf: "" },
-    { codigo: "96150.04", labconf: "" },
-    { codigo: "99402.09", labconf: "" }
+    { codigo: "88141", color:"#D6A99A", labconf: "N" },
+    { codigo: "99402.08", color:"#E16036", labconf: "2" },
+    { codigo: "96150.03", color:"#55868C", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "2" },
+    { codigo: "99402.03", color:"#91C4F2", labconf: "2" },
+    { codigo: "99402.04", color:"#9D79BC", labconf: "2" },
+    { codigo: "C0011", color:"9D79BC", labconf: "2" },
+    { codigo: "99401", color:"#85CB33", labconf: "" }
+  ];
+  valoresAdultoDr2do = [
+    { codigo: "Z000", color:"#D2BF55",  labconf: "" },
+    { codigo: "C8002", color:"#FFEED6", labconf: "TA" },
+    { codigo: "99403.01", color:"#4CE0B3", labconf: "2" },
+    { codigo: "U262", color:"#5465ff", labconf: "DNT" },
+    { codigo: "84152", color:"#edc531", labconf: "RN" },
+    { codigo: "99401.13", color:"#bb8588", labconf: "" },
+    { codigo: "99401", color:"#85CB33", labconf: "2" },
+    { codigo: "96150.02", color:"#FBBFCA", labconf: "2" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "2" },
+    { codigo: "82270", color:"#a3a380", labconf: "N" },
+    { codigo: "U0041", color:"#f85e00", labconf: "" }
+  ];
+  valoresAdultoOtros1er = [
+    { codigo: "88141", color:"#D6A99A", labconf: "N" },
+    { codigo: "88141.01", color:"#ff99c8", labconf: "N" },
+    { codigo: "99402.08", color:"#E16036", labconf: "1" },
+    { codigo: "99402.04", color:"#9D79BC", labconf: "1" },
+    { codigo: "99402.06", color:"#fcf6bd", labconf: "1" },
+    { codigo: "99402.03", color:"#91C4F2", labconf: "1" },
+    { codigo: "99208", color:"#8CA0D7", labconf: "" },
+    { codigo: "90714", color:"#ff97b7", labconf: "1" },
+    { codigo: "90658", color:"#ffb700", labconf: "" },
+    { codigo: "90749.02", color:"#06d6a0", labconf: "" },
+    { codigo: "C0011", color:"9D79BC", labconf: "1" },
+    { codigo: "99401", color:"#85CB33", labconf: "" },
+    { codigo: "96150.04", color:"#6096ba", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" }
   ];
 
   valoresAdulMayorDr1er = [
-    { codigo: "99387", labconf: "AS" },
-    { codigo: "Z636.1", labconf: "" },
-    { codigo: "C8002", labconf: "1" },
-    { codigo: "99401", labconf: "1" },
+    { codigo: "99387", color:"#c200fb", labconf: "AS" },
+    { codigo: "Z636.1", color:"#6a994e", labconf: "" },
+    { codigo: "C8002", color:"#FFEED6", labconf: "1" },
+    { codigo: "99401", color:"#85CB33", labconf: "1" },
     { codigo: "E46X", labconf: "IMC" },
     { codigo: "Z006", labconf: "IMC" },
     { codigo: "E660", labconf: "IMC" },
@@ -174,11 +175,11 @@ export class ExcelsheetComponent implements OnInit {
     { codigo: "U8170", labconf: "RMA" },
     { codigo: "Z010", labconf: "N" },
     { codigo: "99173", labconf: "20" },
-    { codigo: "99401.13", labconf: "" },
+    { codigo: "99401.13", color:"#bb8588", labconf: "" },
     { codigo: "Z011", labconf: "N" },
-    { codigo: "99401.13", labconf: "1" },
+    { codigo: "99401.13", color:"#bb8588", labconf: "1" },
     { codigo: "96150.01", labconf: "" },
-    { codigo: "99402.09", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" },
     { codigo: "Z017", labconf: "" },
     { codigo: "85018", labconf: "1" },
     { codigo: "82947", labconf: "" },
@@ -195,34 +196,34 @@ export class ExcelsheetComponent implements OnInit {
     { codigo: "99199.22", labconf: "N" },
     { codigo: "99386.03", labconf: "N" },
     { codigo: "Z125", labconf: "" },
-    { codigo: "82270", labconf: "" }
+    { codigo: "82270", color:"#a3a380", labconf: "" }
   ];
   valoresAdulMayorOtros1er = [
-    { codigo: "96150.03", labconf: "" },
-    { codigo: "99402.09", labconf: "" },
-    { codigo: "88141", labconf: "" },
-    { codigo: "99402.08", labconf: "1" },
-    { codigo: "90658", labconf: "" },
-    { codigo: "C0011", labconf: "1" }
+    { codigo: "96150.03", color:"#55868C", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "" },
+    { codigo: "88141", color:"#D6A99A", labconf: "" },
+    { codigo: "99402.08", color:"#E16036", labconf: "1" },
+    { codigo: "90658", color:"#ffb700", labconf: "" },
+    { codigo: "C0011", color:"9D79BC", labconf: "1" }
   ];
   valoresAdulMayorDr2do = [
-    { codigo: "99387", labconf: "AS" },
-    { codigo: "Z636.1", labconf: "" },
-    { codigo: "C8002", labconf: "TA" },
-    { codigo: "99401", labconf: "2" },
-    { codigo: "99403.01", labconf: "2" },
-    { codigo: "99401.13", labconf: "2" },
-    { codigo: "96150.07", labconf: "" },
-    { codigo: "99402.09", labconf: "2" },
-    { codigo: "84152", labconf: "N" },
-    { codigo: "82270", labconf: "N" }
+    { codigo: "99387", color:"#c200fb", labconf: "AS" },
+    { codigo: "Z636.1", color:"#6a994e", labconf: "" },
+    { codigo: "C8002", color:"#FFEED6", labconf: "TA" },
+    { codigo: "99401", color:"#85CB33", labconf: "2" },
+    { codigo: "99403.01", color:"#4CE0B3", labconf: "2" },
+    { codigo: "99401.13", color:"#bb8588", labconf: "2" },
+    { codigo: "96150.07", color:"#717744", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "2" },
+    { codigo: "84152", color:"#edc531", labconf: "N" },
+    { codigo: "82270", color:"#a3a380", labconf: "N" }
   ];
   valoresAdulMayorOtros2do = [
-    { codigo: "88141", labconf: "" },
-    { codigo: "C011", labconf: "2" },
-    { codigo: "96150.02", labconf: "" },
-    { codigo: "99402.09", labconf: "2" },
-    { codigo: "99402.08", labconf: "2" }
+    { codigo: "88141", color:"#D6A99A", labconf: "" },
+    { codigo: "C011", color:"#ccff66", labconf: "2" },
+    { codigo: "96150.02", color:"#FBBFCA", labconf: "" },
+    { codigo: "99402.09", color:"#FF8E72", labconf: "2" },
+    { codigo: "99402.08", color:"#E16036", labconf: "2" }
   ];
 
   flagJoven: boolean = false;
@@ -319,19 +320,19 @@ export class ExcelsheetComponent implements OnInit {
   compararJoven2da(codigo: any): string{
     let var1 = "NO";
     let valoresRepe = [
-      { codigo: "Z000", contador: 0 },
-      { codigo: "C8002", contador: 0 },
-      { codigo: "99403.01", contador: 0 },
-      { codigo: "99401", contador: 0 },
+      { codigo: "Z000", color:"#D2BF55",  contador: 0 },
+      { codigo: "C8002", color:"#FFEED6", contador: 0 },
+      { codigo: "99403.01", color:"#4CE0B3", contador: 0 },
+      { codigo: "99401", color:"#85CB33", contador: 0 },
       { codigo: "99173", contador: 0 },
-      { codigo: "99208", contador: 0 },
-      { codigo: "99402.04", contador: 0 },
-      { codigo: "99402.03", contador: 0 },
-      { codigo: "88141", contador: 0 },
-      { codigo: "99402.08", contador: 0 },
-      { codigo: "C011", contador: 0 },
-      { codigo: "99401", contador: 0 },
-      { codigo: "99402.09", contador: 0 }
+      { codigo: "99208", color:"#8CA0D7", contador: 0 },
+      { codigo: "99402.04", color:"#9D79BC", contador: 0 },
+      { codigo: "99402.03", color:"#91C4F2", contador: 0 },
+      { codigo: "88141", color:"#D6A99A", contador: 0 },
+      { codigo: "99402.08", color:"#E16036", contador: 0 },
+      { codigo: "C011", color:"#ccff66", contador: 0 },
+      { codigo: "99401", color:"#85CB33", contador: 0 },
+      { codigo: "99402.09", color:"#FF8E72", contador: 0 }
     ];
 
     this.codigosRealizados.forEach((e:any) =>{
@@ -357,21 +358,21 @@ export class ExcelsheetComponent implements OnInit {
   compararAdulto2da(codigo: any): string{
     let var1 = "NO";
     let valoresRepe = [
-      { codigo: "Z000", contador: 0 },
-      { codigo: "C8002", contador: 0 },
-      { codigo: "99403.01", contador: 0 },
-      { codigo: "84152", contador: 0 },
-      { codigo: "99401.13", contador: 0 },
-      { codigo: "99401", contador: 0 },
-      { codigo: "99402.09", contador: 0 },
-      { codigo: "82270", contador: 0 },
-      { codigo: "88141", contador: 0 },
-      { codigo: "99402.08", contador: 0 },
-      { codigo: "99402.09", contador: 0 },
-      { codigo: "99402.03", contador: 0 },
-      { codigo: "99402.04", contador: 0 },
-      { codigo: "C0011", contador: 0 },
-      { codigo: "99401", contador: 0 }
+      { codigo: "Z000", color:"#D2BF55",  contador: 0 },
+      { codigo: "C8002", color:"#FFEED6", contador: 0 },
+      { codigo: "99403.01", color:"#4CE0B3", contador: 0 },
+      { codigo: "84152", color:"#edc531", contador: 0 },
+      { codigo: "99401.13", color:"#bb8588", contador: 0 },
+      { codigo: "99401", color:"#85CB33", contador: 0 },
+      { codigo: "99402.09", color:"#FF8E72", contador: 0 },
+      { codigo: "82270", color:"#a3a380", contador: 0 },
+      { codigo: "88141", color:"#D6A99A", contador: 0 },
+      { codigo: "99402.08", color:"#E16036", contador: 0 },
+      { codigo: "99402.09", color:"#FF8E72", contador: 0 },
+      { codigo: "99402.03", color:"#91C4F2", contador: 0 },
+      { codigo: "99402.04", color:"#9D79BC", contador: 0 },
+      { codigo: "C0011", color:"9D79BC", contador: 0 },
+      { codigo: "99401", color:"#85CB33", contador: 0 }
     ];
 
     this.codigosRealizados.forEach((e:any) =>{
@@ -395,18 +396,18 @@ export class ExcelsheetComponent implements OnInit {
   compararAdulMayor2da(codigo: any): string{
     let var1 = "NO";
     let valoresRepe = [
-      { codigo: "99387", contador: 0 },
-      { codigo: "Z636.1", contador: 0 },
-      { codigo: "C8002", contador: 0 },
-      { codigo: "99401", contador: 0 },
-      { codigo: "99403.01", contador: 0 },
-      { codigo: "99401.13", contador: 0 },
-      { codigo: "99402.09", contador: 0 },
-      { codigo: "82270", contador: 0 },
-      { codigo: "88141", contador: 0 },
-      { codigo: "C011", contador: 0 },
-      { codigo: "99402.09", contador: 0 },
-      { codigo: "99402.08", contador: 0 }
+      { codigo: "99387", color:"#c200fb", contador: 0 },
+      { codigo: "Z636.1", color:"#6a994e", contador: 0 },
+      { codigo: "C8002", color:"#FFEED6", contador: 0 },
+      { codigo: "99401", color:"#85CB33", contador: 0 },
+      { codigo: "99403.01", color:"#4CE0B3", contador: 0 },
+      { codigo: "99401.13", color:"#bb8588", contador: 0 },
+      { codigo: "99402.09", color:"#FF8E72", contador: 0 },
+      { codigo: "82270", color:"#a3a380", contador: 0 },
+      { codigo: "88141", color:"#D6A99A", contador: 0 },
+      { codigo: "C011", color:"#ccff66", contador: 0 },
+      { codigo: "99402.09", color:"#FF8E72", contador: 0 },
+      { codigo: "99402.08", color:"#E16036", contador: 0 }
     ];
 
     this.codigosRealizados.forEach((e:any) =>{
@@ -420,7 +421,8 @@ export class ExcelsheetComponent implements OnInit {
         var1 += ' , SOLO 1 VEZ';
         valoresRepe.forEach((e) => {
           if(e.codigo == codigo) e.contador += e.contador
-          if(e.contador == 2) var1 += ' , SI 2 VECES';
+          var1 += ' , APARECE '+e.contador+' VECES';
+          // if(e.contador == 2) var1 += ' , SI 2 VECES';
         });
       }
     })
